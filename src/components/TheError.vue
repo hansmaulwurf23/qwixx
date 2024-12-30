2d6ac8b6fe8ecd8c278b2bd982596f5f7d21afa1
unrz266 <martin.fischer@fau.de>
2024-12-30 17:41:31 +0100

    fixed alert catching click event

--- src/components/TheError.vue
+++ src/components/TheError.vue
@@ -4,8 +4,15 @@ const store = useBoardStore()
 </script>
 
 <template>
-  <div id="myErrorAlert" @click="store.unsetError()" class="w-50 m-auto mt-4" :class="{'show': store.currentError}" role="alert">
+  <div id="myErrorAlert" @click="store.unsetError()" class="w-50 m-auto mt-4 show"
+       :class="{'show': store.currentError, 'disnone': !store.currentError}" role="alert">
     <strong>Fehler:</strong>&nbsp;{{ store.currentError }}
     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
   </div>
 </template>
+
+<style scoped>
+.disnone {
+  display: none;
+}
+</style>
