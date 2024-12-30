36642721b01d947da5a54ec25e4f53a864560b12
unrz266 <martin.fischer@fau.de>
2024-12-30 08:22:24 +0100

    colors and design of original score card

--- src/components/TheBoard.vue
+++ src/components/TheBoard.vue
@@ -8,16 +8,21 @@ const store = useBoardStore();
 <template>
   <div id="boardContainer">
     <div v-for="(colNumbers, cidx) in store.numbers" class="p-1 m-1 panelrow" :class="['bg-' + store.colors[cidx]]">
-      <button @click="store.markNumber(cidx, i)"
-              v-for="(v, i) in colNumbers"
-              :class="[v ? 'btn-secondary' : 'btn-light', 'text-' + store.colors[cidx]]"
-              class="squarebtn m-1">{{ cidx < 2 ? i + 2 : 12 - i }}
-      </button>
-      <button @click="store.markLock(cidx)"
-              :class="[store.locks[cidx] ? 'btn-secondary' : 'btn-light', 'text-' + store.colors[cidx]]"
-              class="squarebtn m-1 rounded-circle">
-        <svg-icon type="mdi" :path="store.locks[cidx] ? mdiLock : mdiLockOpen"></svg-icon>
-      </button>
+      <div class="panelrow rounded-2" :class="['bg-' + store.colors[cidx] + '-subtle']">
+        <button @click="store.markNumber(cidx, i)"
+                v-for="(v, i) in colNumbers"
+                :class="[v ? 'btn-secondary' : 'btn-light', v ? 'text-white' : 'text-' + store.colors[cidx], i > 0 ? 'ms-0': 'ms-1']"
+                class="squarebtn m-1">{{ cidx < 2 ? i + 2 : 12 - i }}
+        </button>
+      </div>
+      <span style="width: 15px; height: 5px;" :class="['bg-' + store.colors[cidx] + '-subtle']"></span>
+      <div class="panelrow rounded-circle" :class="['bg-' + store.colors[cidx] + '-subtle']">
+        <button @click="store.markLock(cidx)"
+                :class="[store.locks[cidx] ? 'btn-secondary' : 'btn-light', store.locks[cidx] ? 'text-white' : 'text-' + store.colors[cidx]]"
+                class="squarebtn m-1 rounded-circle">
+          <svg-icon type="mdi" :path="store.locks[cidx] ? mdiLock : mdiLockOpen"></svg-icon>
+        </button>
+      </div>
     </div>
     <div class="panelrow">
       <div class="rounded-2 border border-light legend ms-2">
@@ -41,13 +46,15 @@ const store = useBoardStore();
     <div class="p-1 m-1 panelrow scorerow">
       Ergebnis
       <template v-for="(color, cidx) in store.colors">
-        <div class="mx-2 rounded-2 squarebtn bg-light border border-2 align-middle" :class="['border-' + color]"><p>
+        <div class="mx-2 rounded-2 squarebtn bg-light border border-3 align-middle" :class="['border-' + color]"><p>
           {{ store.getColorScore(cidx) }}</p></div>
         {{ cidx < 3 ? '+' : '-' }}
       </template>
-      <div class="mx-2 rounded-2 squarebtn bg-light border border-2 border-secondary"><p>{{ store.getFailPoints() }}</p></div>
+      <div class="mx-2 rounded-2 squarebtn bg-light border border-3 border-secondary"><p>{{ store.getFailPoints() }}</p>
+      </div>
       =
-      <div class="mx-2 rounded-2 squarebtn bg-light border border-2 border-secondary"><p>{{ store.getScore() }}</p></div>
+      <div class="mx-2 rounded-2 squarebtn bg-light border border-3 border-secondary"><p>{{ store.getScore() }}</p>
+      </div>
     </div>
   </div>
 </template>
